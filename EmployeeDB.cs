d6c194f0e5a4295ddc10a6ea3e3936a298f33655
Muneebbhatti465 <103245544+Muneebbhatti465@users.noreply.github.com>
2023-07-02 23:07:25 +0500

    Add files via upload

---
+++ EmployeeDB.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+namespace Muneeb.Models
+{
+    public class EmployeeDB
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> Employeelist = new List<Employee>();
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("SPGet",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Employee emp = new Employee();
+                emp.Id = Convert.ToInt32 (dr.GetValue(0).ToString());
+                emp.Name= dr.GetValue(1).ToString();
+                emp.FatherName = dr.GetValue(2).ToString();
+                Employeelist.Add(emp);
+            }
+            con.Close();
+
+
+            return Employeelist;
+        }
+
+        public bool AddEmployee(Employee emp)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("spaddemployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", emp.Name);
+            cmd.Parameters.AddWithValue("@FatherName", emp.FatherName);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateEmployee(Employee emp)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("SpUpdateEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", emp.Id);
+            cmd.Parameters.AddWithValue("@Name", emp.Name);
+            cmd.Parameters.AddWithValue("@FatherName", emp.FatherName);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Muneeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Muneeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            EmployeeDB db = new EmployeeDB();
+            List<Employee> obj = db.GetEmployees();
+
+            return View(obj);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid == true)
+            {
+                EmployeeDB context = new EmployeeDB();
+              bool check =   context.AddEmployee(emp);
+                if (check == true)
+                {
+                    TempData["InsertMessage"] = "Data has been submited";
+                    ModelState.Clear();
+                    return RedirectToAction("Index");
+
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            EmployeeDB context = new EmployeeDB();
+            var row = context.GetEmployees().Find(model => model.Id == Id);
+
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int Id, Employee emp)
+        {
+            if (ModelState.IsValid == true) 
+            {
+                EmployeeDB context = new EmployeeDB();
+                bool check = context.UpdateEmployee(emp);
+                if (check == true)
+                {
+                    TempData["UpdateMessage"] = "Data has been Updated";
+                    ModelState.Clear();
+                    return RedirectToAction("Index");
+
+                }
+
+              
+        }
+            return View();
+        }
+}
